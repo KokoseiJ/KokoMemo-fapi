d1cedd759b7fd522282f863ec917b9f49050ee9e
Wonjun Jung <kokoseij@gmail.com>
2024-05-02 13:07:44 -0700

    app: configure logger

---
+++ kokomemo/logger.py
@@ -0,0 +1,27 @@
+from .config import config
+import logging
+from logging.handlers import StreamHandler, FileHandler
+
+logger = logging.getLogger(config.app_name)
+logger.setLevel(config.loglevel)
+
+asyncio_logger = logging.getLogger("asyncio")
+
+formatter = logging.Formatter(
+    fmt="%(levelname)s | %(asctime)s | "
+    "%(funcName)s (%(filename)s:%(lineno)d): %(message)s"
+)
+
+stream_handler = StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+asyncio_logger.addHandler(stream_handler)
+
+if config.logfile:
+    file_handler = FileHandler(config.logfile, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+else:
+    file_handler = None
