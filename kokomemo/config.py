9939c5c39fd231069f7058bd81e33fdf0574d436
Wonjun Jung <kokoseij@gmail.com>
2024-05-06 11:04:01 -0700

    db: implement session related db utils

--- kokomemo/config.py
+++ kokomemo/config.py
@@ -7,8 +7,21 @@ class Settings(BaseSettings):
     dbname: str = app_name
     logfile: str = None
     loglevel: str = "INFO"
+    # Secret key for JWT signing- I recommend 512bit or larger!
+    secret: str
+    google_id: str
+    # Access Token TTL: 30 minutes
+    access_ttl: int = 1800
+    # Refresh Token TTL: 1 day
+    refresh_ttl: int = 86400
+    # Refresh Token Persisting session TTL: 1 week
+    refresh_persist_ttl: int = 604800
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
 config = Settings()
+
+
+def get_config() -> Settings:
+    return config
